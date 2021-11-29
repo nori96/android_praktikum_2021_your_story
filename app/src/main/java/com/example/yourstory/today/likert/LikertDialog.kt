41be363af118d86ecaf224139a3653de47fa167d
BenjaminStahr <BenjaminStahr97@web.de>
2021-11-29 21:58:09 +0100

    adjusted text coloring, first version of likert dialog

--- app/src/main/java/com/example/yourstory/today/likert/LikertDialog.kt
+++ app/src/main/java/com/example/yourstory/today/likert/LikertDialog.kt
@@ -2,31 +2,47 @@ package com.example.yourstory.today.likert
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.yourstory.R
+import com.example.yourstory.databinding.LikertDialogFragmentBinding
+import com.example.yourstory.databinding.ThoughtDialogFragmentBinding
+import com.example.yourstory.today.thought.SharedThoughtDialogViewModel
 
 class LikertDialog : Fragment() {
 
-    companion object {
-        fun newInstance() = LikertDialog()
-    }
-
-    private lateinit var viewModel: LikertDialogViewModel
+    private lateinit var likertViewModel: LikertDialogViewModel
+    private lateinit var hostFragmentNavController: NavController
+    private var _binding: LikertDialogFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.likert_dialog_fragment, container, false)
-    }
+        _binding = LikertDialogFragmentBinding.inflate(inflater, container, false)
+        likertViewModel = ViewModelProvider(requireActivity())[LikertDialogViewModel::class.java]
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(LikertDialogViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+        hostFragmentNavController = NavHostFragment.findNavController(this)
+        binding.joySeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                //binding.startLikertDialog.text = progress.toString()
+                Log.i("info",progress.toString())
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+        })
+        return binding.root
+    }
 }
