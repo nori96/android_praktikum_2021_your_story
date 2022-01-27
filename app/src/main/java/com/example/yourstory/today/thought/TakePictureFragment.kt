18dd315600937c6ba28799c68757dc9e788c983c
Erik Berwald <erik.berwald@uol.de>
2022-01-27 14:24:45 +0100

    + changed TakePictureFragment to CameraX

--- app/src/main/java/com/example/yourstory/today/thought/TakePictureFragment.kt
+++ app/src/main/java/com/example/yourstory/today/thought/TakePictureFragment.kt
@@ -1,32 +1,50 @@
 package com.example.yourstory.today.thought
 
+import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
 import android.content.ActivityNotFoundException
+import android.content.ContentValues.TAG
 import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.media.Image
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
+import android.util.Rational
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.yourstory.R
 import com.example.yourstory.databinding.TakePictureFragmentBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kotlinx.android.synthetic.main.take_picture_fragment.*
+import java.io.ByteArrayOutputStream
+import java.io.File
 import java.nio.ByteBuffer
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import java.util.jar.Manifest
 
 class TakePictureFragment : Fragment(){
 
     private lateinit var viewModelShared: SharedThoughtDialogViewModel
     private lateinit var hostFragmentNavController: NavController
-    private var _binding: TakePictureFragmentBinding? = null
-    private lateinit var imageView: ImageView
-    private lateinit var imageBitmap: Bitmap
-    private val binding get() = _binding!!
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+    private var imageCapture: ImageCapture? = null
+    private lateinit var binding: TakePictureFragmentBinding
     lateinit var materialAlertDialogBuilder: MaterialAlertDialogBuilder
     val REQUEST_IMAGE_CAPTURE = 1
 
@@ -34,84 +52,84 @@ class TakePictureFragment : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = TakePictureFragmentBinding.inflate(inflater, container, false)
+        binding = TakePictureFragmentBinding.inflate(inflater, container, false)
         hostFragmentNavController = NavHostFragment.findNavController(this)
 
         viewModelShared = ViewModelProvider(requireActivity())[SharedThoughtDialogViewModel::class.java]
 
-        //Get the ImageView
-        imageView = _binding!!.pictureView
+        startCamera();
 
-        materialAlertDialogBuilder = MaterialAlertDialogBuilder(requireContext())
-
-        //Setup Confirm-Button
-        binding.confirmThoughtDialogPicture.setOnClickListener {
-            if(!viewModelShared.picSelected.value!!){
-                materialAlertDialogBuilder.setTitle(R.string.take_a_picture_title)
-                materialAlertDialogBuilder.setMessage(R.string.take_a_picture_text)
-                materialAlertDialogBuilder.setPositiveButton("OK"){
-                    dialog, which ->
-                }
-                materialAlertDialogBuilder.show()
-            }else {
-                viewModelShared.image = viewModelShared.pictureImage
-                hostFragmentNavController.navigate(R.id.action_takePictureFragment_to_thought_dialog)
-            }
+        // Set up the listener for take photo button
+        binding.cameraCaptureButton?.setOnClickListener {
+            takePhoto();
         }
-
-        binding.takeNewPictureButton.setOnClickListener{
-            dispatchTakePictureIntent()
-        }
-
-        binding.cancelThoughtDialogPicture.setOnClickListener {
-            hostFragmentNavController.navigate(R.id.action_takePictureFragment_to_thought_dialog)
-        }
-
-        viewModelShared.pictureImage.observe(viewLifecycleOwner,{
-            newBitmap -> imageView.setImageBitmap(newBitmap)
-        })
-
+        cameraExecutor = Executors.newSingleThreadExecutor()
         return binding.root
     }
-    private fun dispatchTakePictureIntent() {
-        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-        } catch (e: ActivityNotFoundException) {
-            // display error state to the user
-        }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder?.surfaceProvider)
+                }
+
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+
+            //TODO:Define Aspect Ratio here!
+            //imageCapture!!.setCropAspectRatio(Rational(16,9))
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            imageBitmap = data!!.extras!!.get("data") as Bitmap
-            viewModelShared.picSelected.value = true
-            viewModelShared.pictureImage.value = imageBitmap
-        }
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+           ContextCompat.getMainExecutor(requireContext()), object :
+                ImageCapture.OnImageCapturedCallback() {
+
+                @SuppressLint("UnsafeOptInUsageError")
+                override fun onCaptureSuccess(image: ImageProxy) {
+                }
+                override fun onError(error: ImageCaptureException)
+                {
+                    // insert your code here.
+                }
+           })
     }
 
-
-    /**
-     * Convert bitmap to byte array using ByteBuffer.
-     */
-    fun Bitmap.convertToByteArray(): ByteArray {
-        //minimum number of bytes that can be used to store this bitmap's pixels
-        val size = this.byteCount
-
-        //allocate new instances which will hold bitmap
-        val buffer = ByteBuffer.allocate(size)
-        val bytes = ByteArray(size)
-
-        //copy the bitmap's pixels into the specified buffer
-        this.copyPixelsToBuffer(buffer)
-
-        //rewinds buffer (buffer position is set to zero and the mark is discarded)
-        buffer.rewind()
-
-        //transfer bytes from buffer into the given destination array
-        buffer.get(bytes)
-
-        //return bitmap's pixels
-        return bytes
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
