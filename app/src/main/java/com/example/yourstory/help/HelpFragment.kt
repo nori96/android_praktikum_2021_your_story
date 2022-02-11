b817da527fd022719f5303ad89c2550aff226eb8
BenjaminStahr <BenjaminStahr97@web.de>
2022-02-11 19:08:51 +0100

    help page implemented

--- app/src/main/java/com/example/yourstory/help/HelpFragment.kt
+++ app/src/main/java/com/example/yourstory/help/HelpFragment.kt
@@ -2,31 +2,25 @@ package com.example.yourstory.help
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.yourstory.R
+import com.example.yourstory.databinding.HelpFragmentBinding
+import com.example.yourstory.databinding.ReportsFragmentBinding
 
 class HelpFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = HelpFragment()
-    }
-
-    private lateinit var viewModel: HelpViewModel
+    private lateinit var binding: HelpFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.help_fragment, container, false)
+    ): View {
+        binding = HelpFragmentBinding.inflate(inflater, container, false)
+        binding.helpPageContentText.movementMethod = ScrollingMovementMethod()
+        return binding.root
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HelpViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/example/yourstory/help/HelpViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.yourstory.help
-
-import androidx.lifecycle.ViewModel
-
-class HelpViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
